59184142024ed22071b320ea7a48f6c90660eb92
Joseph Perl <jiperl@topasmc.org>
2024-01-12 15:50:01 -0800

    update

--- .github/workflows/install-topas.sh
+++ .github/workflows/install-topas.sh
@@ -5,5 +5,5 @@ set -e
 
 cd "$TOPAS_SOURCE_PATH" || exit
 unzip Geant4Headers.zip > /dev/null
-cmake -DTOPAS_EXTENSIONS_DIR="$NBIO_SOURCE_PATH"
+cmake -DTOPAS_EXTENSIONS_DIR="$NBIO_SOURCE_PATH" -DCMAKE_INSTALL_RPATH "\$ORIGIN/../lib"
 make
