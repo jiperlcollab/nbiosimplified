a5149035fdb89ba106f74fd2cf2b22d353ff0c82
Joseph Perl <jiperl@topasmc.org>
2024-01-23 14:54:43 -0800

    cleaned

---
+++ myextensions/MyComponent1.cc
@@ -0,0 +1,64 @@
+// Component for MyComponent1
+//
+// ********************************************************************
+// *                                                                  *
+// * Copyright 2022 The TOPAS Collaboration                           *
+// *                                                                  *
+// * Permission is hereby granted, free of charge, to any person      *
+// * obtaining a copy of this software and associated documentation   *
+// * files (the "Software"), to deal in the Software without          *
+// * restriction, including without limitation the rights to use,     *
+// * copy, modify, merge, publish, distribute, sublicense, and/or     *
+// * sell copies of the Software, and to permit persons to whom the   *
+// * Software is furnished to do so, subject to the following         *
+// * conditions:                                                      *
+// *                                                                  *
+// * The above copyright notice and this permission notice shall be   *
+// * included in all copies or substantial portions of the Software.  *
+// *                                                                  *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,  *
+// * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES  *
+// * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND         *
+// * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT      *
+// * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,     *
+// * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
+// * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR    *
+// * OTHER DEALINGS IN THE SOFTWARE.                                  *
+// *                                                                  *
+// ********************************************************************
+//
+
+#include "MyComponent1.hh"
+
+#include "TsParameterManager.hh"
+
+#include "G4Box.hh"
+
+MyComponent1::MyComponent1(TsParameterManager* pM, TsExtensionManager* eM, TsMaterialManager* mM, TsGeometryManager* gM,
+			 TsVGeometryComponent* parentComponent, G4VPhysicalVolume* parentVolume, G4String& name) :
+TsVGeometryComponent(pM, eM, mM, gM, parentComponent, parentVolume, name)
+{
+}
+
+
+MyComponent1::~MyComponent1()
+{
+}
+
+
+G4VPhysicalVolume* MyComponent1::Construct()
+{
+	BeginConstruction();
+
+	G4double totalHLX = fPm->GetDoubleParameter(GetFullParmName("HLX"), "Length");
+	G4double totalHLY = fPm->GetDoubleParameter(GetFullParmName("HLY"), "Length");
+	G4double totalHLZ = fPm->GetDoubleParameter(GetFullParmName("HLZ"), "Length");
+
+	G4Box* envelopeSolid = new G4Box(fName, totalHLX, totalHLY, totalHLZ);
+	fEnvelopeLog = CreateLogicalVolume(envelopeSolid);
+	fEnvelopePhys = CreatePhysicalVolume(fEnvelopeLog);
+	
+	InstantiateChildren(fEnvelopePhys);
+	
+	return fEnvelopePhys;
+}
